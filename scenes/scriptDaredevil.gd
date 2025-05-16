ec60d0eed75387b787532d3c24f11f2909d4ffcc
EscritorioFer <fernandooxz3@gmail.com>
2025-05-16 12:11:58 -0300

    Creando repositorio

---
+++ scenes/scriptDaredevil.gd
@@ -0,0 +1,78 @@
+class_name daredevil
+extends CharacterBody2D
+
+@export var gravity=1200
+@export var jumpspeed=400
+@export var velocidad=400
+@export var aceleracion=1500
+@export var friction=3500
+var facing := 1
+
+
+func _physics_process(delta):
+	
+	#gravedad
+	if not is_on_floor():
+		velocity.y = velocity.y + gravity * delta
+		if velocity.y < 0:
+			#$RedSuite.play("jump")
+			$RedSuite.position.x = 0
+			$RedSuite.position.y = 1
+			$RedSuite.scale.x = facing
+			$RedSuite.scale.y = 1
+	
+	#saltar
+	var jump_pressed = Input.is_action_just_pressed("jump") and is_on_floor()
+	if jump_pressed:
+		velocity.y = velocity.y - jumpspeed
+		
+	var atake = Input.is_action_pressed("attack")
+
+	#move
+	var direction = Input.get_axis("left","right")
+	
+	
+	if is_on_floor():
+		if direction != 0 :
+			if Input.is_action_pressed("run"):
+				velocity.x = move_toward(velocity.x,direction * 2 * velocidad,aceleracion * delta)
+				#$RedSuite.play("run")
+				if direction == 1 :
+					facing= 1
+				elif direction == -1 :
+					facing= -1
+				$RedSuite.position.x = 0
+				$RedSuite.position.y = 0
+				$RedSuite.scale.x = facing
+				$RedSuite.scale.y = 1
+			else:
+				velocity.x = move_toward(velocity.x,direction * velocidad,aceleracion * delta)
+				#$RedSuite.play("walk")
+				if direction == 1 :
+					facing= 1
+				elif direction == -1 :
+					facing= -1
+				$RedSuite.position.x = 0
+				$RedSuite.position.y = 0
+				$RedSuite.scale.x = facing * 0.5
+				$RedSuite.scale.y = 1 * 0.5
+				
+				
+		else:
+			velocity.x=move_toward(velocity.x,0,friction * delta)
+			if atake:
+				$RedSuite.play("hit")
+				$RedSuite.position.x = 0
+				$RedSuite.position.y = 0
+				$RedSuite.scale.x = 1 * facing
+				$RedSuite.scale.y = 1
+				
+			else:
+				$RedSuite.play("idle")
+				$RedSuite.position.x = 0
+				$RedSuite.position.y = 0
+				$RedSuite.scale.x = facing * 2
+				$RedSuite.scale.y = 1 * 2
+			
+		
+	move_and_slide()
