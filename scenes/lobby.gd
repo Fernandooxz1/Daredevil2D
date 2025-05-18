83b6ac1fdd4bf4d2a036af510f4c870439ee3833
Fernando <fernandooxz1gmail.com>
2025-05-18 18:00:51 -0300

    Agregando sonidos a todos los menu

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -5,13 +5,26 @@ func _ready() -> void:
 	if not AudioManager2.get_node("AudioStreamPlayer").playing:
 		AudioManager2.get_node("AudioStreamPlayer").play()
 
+# Boton 1 "Play"
+func _on_button_mouse_entered() -> void:
+	$VBoxContainer/MoveSound.play()
 func _on_button_pressed() -> void:
+	$VBoxContainer/SelectSound.play()
+	await get_tree().create_timer(0.15).timeout	
 	get_tree().change_scene_to_file("res://scenes/lvl_1.tscn")
 
-
+# Boton 2 "Options"
+func _on_button_2_mouse_entered() -> void:
+	$VBoxContainer/MoveSound.play()
 func _on_button_2_pressed() -> void:
+	$VBoxContainer/SelectSound.play()
+	await get_tree().create_timer(0.15).timeout	
 	get_tree().change_scene_to_file("res://scenes/Options.tscn")
 
-
+# Boton 3 "Quit"
+func _on_button_3_mouse_entered() -> void:
+	$VBoxContainer/MoveSound.play()
 func _on_button_3_pressed() -> void:
+	$VBoxContainer/SelectSound.play()
+	await get_tree().create_timer(0.15).timeout	
 	get_tree().quit()
