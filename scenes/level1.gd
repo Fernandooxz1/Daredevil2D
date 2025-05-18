69255df8d7e5e11bc3ebbab958b0481b27df4252
Marco_Ortiz <marco.ortiz@alu.ing.unlp.edu.ar>
2025-05-18 03:18:45 -0300

    muerte

--- scenes/level1.gd
+++ scenes/level1.gd
@@ -3,7 +3,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
+	var health_comp = $Daredevil/HealthComponent
+	var DD = $Daredevil
+	var JuD = $Daredevil/Camera2D2/HUD
+	
+	health_comp.connect("onHealthChanged", Callable(JuD, "_on_health_changed"))
+	health_comp.connect("onDead", Callable(DD,"_on_dead"))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
