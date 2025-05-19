525df0aa327d963c74185c68e334d2977bb3bfcf
Marco_Ortiz <marco.ortiz@alu.ing.unlp.edu.ar>
2025-05-19 01:38:11 -0300

    opciones en pausa y muerte

---
+++ scenes/options2.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+# Boton 1
+func _on_button_4_mouse_entered() -> void:
+	$MoveSound2.play()
+func _on_button_4_pressed() -> void:
+	$SelectSound2.play()
+
+# Boton 2
+func _on_button_5_mouse_entered() -> void:
+	$MoveSound2.play()
+func _on_button_5_pressed() -> void:
+	$SelectSound2.play()
+
+# Boton 3
+func _on_button_6_mouse_entered() -> void:
+	$MoveSound2.play()
+func _on_button_6_pressed() -> void:
+	$SelectSound2.play()
--- scenes/pausa.gd
+++ scenes/pausa.gd
@@ -4,8 +4,19 @@ extends CanvasLayer
 func _physics_process(delta):
 	if Input.is_action_just_pressed("Pausa"):
 		get_tree().paused = not get_tree().paused
-		$TextureRect.visible = not $TextureRect.visible
-		$VBoxContainer.visible = not $VBoxContainer.visible
+		if $TextureRect.visible == true:
+			$TextureRect.visible = false
+		else : $TextureRect.visible = true
+		
+		if $Menu1.visible == true:
+			$Menu1.visible = false
+		elif $options.visible == true:
+			$options.visible = false 
+		else : $Menu1.visible = true
+		
+		if $options.visible == true:
+			$options.visible = false
+			
 		if $TextureRect.visible == false:
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		elif $TextureRect.visible == true: Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -14,10 +25,24 @@ func _physics_process(delta):
 func _on_button_pressed() -> void:
 		get_tree().paused = not get_tree().paused
 		$TextureRect.visible = not $TextureRect.visible
-		$VBoxContainer.visible = not $VBoxContainer.visible
+		$Menu1.visible = not $Menu1.visible
 		if $TextureRect.visible == false:
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		elif $TextureRect.visible == true: Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
+func _on_button_2_pressed() -> void:
+	$Menu1.visible = not $Menu1.visible
+	$options.visible = not $options.visible
+
 func _on_button_3_pressed() -> void:
 	get_tree().quit()
+	
+func _on_button_4_pressed() -> void:
+	pass
+
+func _on_button_5_pressed() -> void:
+	pass
+
+func _on_button_6_pressed() -> void:
+	$Menu1.visible = not $Menu1.visible
+	$options.visible = not $options.visible
